c339362f97901fdb1ba281ed281538f8b908fbc4
yupinggao <46269261+yupinggao@users.noreply.github.com>
2019-12-16 13:00:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Yuping!");
+    response.end("Hello Yuping 2!");
 });
 
 const port = process.env.PORT || 1337;
